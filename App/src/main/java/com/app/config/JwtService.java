1f96211fcb618ff010d5b052ceb23740bed5cf90
Mensaloveable <abdullahi.adedayo@gmail.com>
2024-04-14 19:56:31 +0100

    feat: add swagger & spring security

---
+++ App/src/main/java/com/app/config/JwtService.java
@@ -0,0 +1,95 @@
+package com.app.config;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.MalformedJwtException;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.impl.DefaultClaims;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Service;
+
+import java.security.Key;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.function.Function;
+
+@Slf4j
+@Service
+@RequiredArgsConstructor
+public class JwtService {
+
+    private static final long EXPIRATION_TIME_MS = 600_000L;
+
+    @Value("${app.jwt.secret}")
+    String SECRET_KEY;
+
+    public String extractUsername(String token) {
+        return extractAllClaims(token, Claims::getSubject);
+    }
+
+    public <T> T extractAllClaims(String token, Function<Claims, T> claimResolver) {
+        final Claims claims = extractAllClaims(token);
+        return claimResolver.apply(claims);
+    }
+
+    public String generateToken(UserDetails userDetails) {
+        return generateToken(new HashMap<>(), userDetails);
+    }
+
+    public String generateToken(Map<String, Object> extractClaims, UserDetails userDetails) {
+
+        return Jwts
+                .builder()
+                .setClaims(extractClaims)
+                .setSubject(userDetails.getUsername())
+                .setIssuedAt(new Date(System.currentTimeMillis()))
+                .setExpiration(new Date(System.currentTimeMillis() + EXPIRATION_TIME_MS))
+                .signWith(getSignInKey(), SignatureAlgorithm.HS256)
+                .compact();
+    }
+
+
+    public boolean isTokenValid(String token, UserDetails userDetails) {
+        final String username = extractUsername(token);
+        return (username.equals(userDetails.getUsername())) && !isTokenExpired(token);
+    }
+
+    private boolean isTokenExpired(String token) {
+        return extractExpiration(token).before(new Date());
+    }
+
+    private Date extractExpiration(String token) {
+        return extractAllClaims(token, Claims::getExpiration);
+    }
+
+    private Claims extractAllClaims(String token) {
+        try {
+
+            return Jwts
+                    .parser()
+                    .setSigningKey(getSignInKey())
+                    .build()
+                    .parseClaimsJws(token)
+                    .getBody();
+
+        } catch (MalformedJwtException e) {
+
+            log.error("Invalid JWT token: {}", e.getMessage());
+        } catch (Exception e) {
+
+            log.error("Unexpected error during JWT parsing: {}", e.getMessage());
+        }
+        return null;
+    }
+
+    private Key getSignInKey() {
+        byte[] keyBytes = Decoders.BASE64.decode(SECRET_KEY);
+        return Keys.hmacShaKeyFor(keyBytes);
+    }
+}
---
+++ App/src/main/java/com/app/config/SpringdocConfig.java
@@ -0,0 +1,31 @@
+package com.app.config;
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@OpenAPIDefinition
+@Configuration
+public class SpringdocConfig {
+    @Bean
+    public OpenAPI baseOpenApi() {
+        final String securitySchemaName = "bearerAuth";
+        return new OpenAPI()
+                .info(new Info()
+                        .title("Skool Academy Portal API")
+                        .version("1.0.0")
+                        .description("Skool Academy Portal"))
+                .addSecurityItem(new SecurityRequirement().addList(securitySchemaName))
+                .components(new Components()
+                        .addSecuritySchemes(securitySchemaName, new SecurityScheme()
+                                .name(securitySchemaName)
+                                .type(SecurityScheme.Type.HTTP)
+                                .scheme("bearer")
+                                .bearerFormat("JWT")));
+    }
+}
